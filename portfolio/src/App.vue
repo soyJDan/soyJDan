b84c9731cba7664a0f4fd60a12e1490369aeb63d
soyJDan <soyjdan@git.co>
2023-12-12 11:39:43 +0100

    Added project with Vue
    Added folder portfolio with Framework Vue.

---
+++ portfolio/src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import Header from './components/Header.vue'
+import About from '@/components/About.vue'
+import Home from '@/components/Home.vue'
+
+const siteProps = {
+  name: "Daniel Romero",
+  title: "Software Developer",
+  email: "juandanielromerobedoya@gmail.com",
+  github: "soyJDan",
+  twitter: "soyJDan",
+  linkedin: "soyJDan",
+}
+</script>
+
+<template>
+  <header>
+    <Header />
+  </header>
+
+  <main>
+    <Home />
+    <About />
+
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+}
+</style>
---
+++ portfolio/src/components/Home.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+  const urlImage = "https://i.imgur.com/gOmSTjv.png";
+  const altImage = "Kermit The Frog";
+
+</script>
+
+<template>
+  <div id="home">
+    <img src="https://i.imgur.com/gOmSTjv.png" alt={{altImage}} />
+
+    <div class="title">
+      <h1>Da</h1>
+      <h2>SF</h2>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .title {
+    position: absolute;
+    top: 5rem;
+    left: 2rem;
+    width: 17rem;
+  }
+</style>
